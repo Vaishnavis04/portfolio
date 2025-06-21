ac9f4a7245e8244dc90586e911c72a5b327122b4
Vaishnavis04 <136159499+Vaishnavis04@users.noreply.github.com>
2025-06-21 21:58:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import HomePage from './Pages/Home/HomeDesktop';
-import Icons from './Pages/Icons';
-import Project from './Pages/Project/ProjectDesktop';
-import Skills from './Pages/Skills/Skills';
-import Contact from './Pages/Contact/Contact';
-import Footer from './Pages/Footer/Footer'
+// import Icons from './Pages/Icons';
+// import Project from './Pages/Project/ProjectDesktop';
+// import Skills from './Pages/Skills/Skills';
+// import Contact from './Pages/Contact/Contact';
+// import Footer from './Pages/Footer/Footer'
 
 function App() {
   return (
